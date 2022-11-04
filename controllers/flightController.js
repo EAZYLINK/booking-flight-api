816a348a827bb99311ca5695327d0d37e641be84
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2022-11-04 17:47:28 +0100

    book flight feature created

--- controllers/flightController.js
+++ controllers/flightController.js
@@ -1,6 +1,28 @@
-exports.example = (req, res) => {
-    console.log("example")
-    res.send("Flight example")
-}
+const fs = require('fs')
 
+// exports.example = (req, res) => {
+//     console.log("example")
+//     res.send("Flight example")
+// }
+exports.bookFlight = (req, res) => {
+    const {title, price, time, date} = req.body
+  if (!title || !price || !time || !date)
+  res.status(400).json("Fill all the required field");  
 
+     const flight = fs.readFileSync("./models/flight.json");
+
+        var flightToJson = JSON.parse(flight);
+        console.log(flightToJson)
+        const data = {title, price, time, date}
+        flightToJson.push(data)
+        const newFlght = JSON.stringify(flightToJson);
+
+        fs.writeFile("./models/flight.json", newFlght, (err) => {
+          if (err) {
+            res.status(500).json(err);
+            return;
+          }
+          res.status(201).json("Your account has been created successfully");
+        });
+  };
+  
--- index.js
+++ index.js
@@ -3,12 +3,25 @@ const { json } = require("express");
 const flights = require("./controllers/flightController");
 const models = require("./models/Flight");
 const routes = require("./routes/flightRoute");
+const fs = require('fs')
 
 const app = express();
 
 app.use(json());
 
-app.use("/", routes);
+app.use("/api/v1/flight", routes);
+
+app.get('/api/v1/flight', (req, res)=>{
+  fs.readFile('./models/flight.json', 'utf8', (err, flight)=>{
+    if (err) throw err
+      res.status(201).json({
+      success: true,
+      Message: "flight fetched successfully",
+      Flight_Details: JSON.parse(flight)
+  })
+  console.log(data)
+    })
+})
 
 const port = process.env.PORT || 3000;
 
